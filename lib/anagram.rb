1fcb6df244f51cc3d831c971a37c278bfd6c432b
Bry Cox <carl.cc@gmail.com>
2019-02-18 23:57:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ def match(arr)
  var_2 = []
  var = data.split("").sort
  var_1 = arr.collect {|x| x.split("").sort}
- var_1.each_with_index {|x, i| var_2 << arr[i] if x == var}
+ var_1.each_with_index.collect {|x, i| var_2 << arr[i] if x == var}
  var_2
 end
 end
