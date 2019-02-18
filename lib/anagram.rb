d20b498925ea172eacd8acd23af244e8f5ad6fcc
Bry Cox <carl.cc@gmail.com>
2019-02-18 22:40:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,5 @@
 # Your code goes here!
-binding.pry
+require 'pry'
 class Anagram
   attr_accessor :data
 
@@ -10,6 +10,7 @@ end
 def match(data)
   var = self.new
   var.data.split("")
+  binding.pry
 
 end
 
